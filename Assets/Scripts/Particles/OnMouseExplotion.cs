7d48c4f0e8199a9a3b2c0599e52a4a1fc13a352e
Lukas <63304771+Lukas-14@users.noreply.github.com>
2024-01-30 09:25:06 +0100

    refactored

--- Assets/Scripts/Particles/OnMouseExplotion.cs
+++ Assets/Scripts/Particles/OnMouseExplotion.cs
@@ -14,11 +14,14 @@ public class OnMouseExplotion : MonoBehaviour
             // Convert mouse position to world position
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             worldPosition.z = 0; // Assuming you want the particle system in the XY plane
-
-            // Instantiate the particle system with the same rotation as the original
-            ParticleSystem ps = Instantiate(particleSystem, worldPosition, particleSystem.transform.rotation) as ParticleSystem;
-            ps.Play();
-            Destroy(ps.gameObject, 2); // Destroy after 3 seconds
+            Particle(particleSystem, worldPosition);
         }
+    }
+    void Particle(ParticleSystem particleSystem,Vector3 possitionOfParticalEffect)
+    {
+        // Instantiate the particle system with the same rotation as the original
+        ParticleSystem ps = Instantiate(particleSystem, possitionOfParticalEffect, particleSystem.transform.rotation) as ParticleSystem;
+        ps.Play();
+        Destroy(ps.gameObject, 2); // Destroy after 3 seconds
     }
 }
