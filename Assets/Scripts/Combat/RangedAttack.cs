056ed209ca4ede1dcd7be6cbe6d331f2ea20a65a
Lukas <63304771+Lukas-14@users.noreply.github.com>
2024-02-20 10:32:55 +0100

    Added particle effects to ranged attack
    also removed the use of enemy stats interface as it didn't work

--- Assets/Scripts/Combat/RangedAttack.cs
+++ Assets/Scripts/Combat/RangedAttack.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
 
-// Denne sørger for at attacket kun er på skermen i 1 sekundt
-// ændre AttackUpTime for at ændre hvor langt tid den er på skærmen
+// Denne sï¿½rger for at attacket kun er pï¿½ skermen i 1 sekundt
+// ï¿½ndre AttackUpTime for at ï¿½ndre hvor langt tid den er pï¿½ skï¿½rmen
 // -Morgan
 
 public class RangedAttack : MonoBehaviour
 {
     [SerializeField] private float AttackUpTime;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] ParticleSystem particleSystem = default; // Assign your particle system in the inspector
     private bool hasHit;
 
     // Start is called before the first frame update
@@ -30,23 +31,32 @@ public class RangedAttack : MonoBehaviour
             Destroy(gameObject);
         }
 
-        if (!hasHit)
-        {
-            AttackHit();
-        }
     }
-
-    void AttackHit()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        Collider2D collider = Physics2D.OverlapCapsule(transform.position, transform.localScale, CapsuleDirection2D.Horizontal, transform.rotation.y);
-        if (collider == null) return;
-        IDamageable damageable = collider.gameObject.GetComponent<IDamageable>();
+        Debug.Log(collision.gameObject.name);
+        // Check if the collision is with a GameObject that implements IDamageable
+        IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
+        Debug.Log(damageable + " damageable");
+    
+        // Check if damageable is not null and it's not the CombatPlayer
         if (damageable != null && damageable != (CombatPlayer.combatPlayer as IDamageable))
         {
-            damageable.TakeDamage(1); // ændre dette så den tage combatPlayerens magical attack
-            hasHit = true;
+            // Assuming you want to access the CombatPlayer's magical attack value, you need to reference it directly
+            // For example, let's say CombatPlayer has a public property or field called 'MagicalAttack'
+
+            damageable.TakeDamage(1);
             Debug.Log(damageable.Health);
-            Destroy(gameObject);
+            Particle(particleSystem, collision.transform.position);
+            Destroy(gameObject); // Destroy the projectile or attacker GameObject
         }
     }
+    void Particle(ParticleSystem particleSystem,Vector3 possitionOfParticalEffect)
+    {
+        // Instantiate the particle system with the same rotation as the original
+        ParticleSystem ps = Instantiate(particleSystem, possitionOfParticalEffect, particleSystem.transform.rotation) as ParticleSystem;
+        ps.Play();
+        Destroy(ps.gameObject, 2); // Destroy after 3 seconds
+    }
+
 }
