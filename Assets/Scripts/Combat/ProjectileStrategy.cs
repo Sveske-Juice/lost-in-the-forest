cbf8640a3c6bdcfd6e680b746469d9efa8b10561
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2024-02-24 13:23:46 +0100

    fix attack direction with new camera follower

--- Assets/Scripts/Combat/ProjectileStrategy.cs
+++ Assets/Scripts/Combat/ProjectileStrategy.cs
@@ -10,7 +10,7 @@ public class ProjectileStrategy : AttackStrategy
 
     public override void Attack(AttackContext _context)
     {
-        Vector3 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - _context.origin.position;
+        Vector3 dir = Camera.main.ScreenToViewportPoint(Input.mousePosition) - Camera.main.WorldToViewportPoint(_context.origin.position);
         dir.z = 0;
         dir.Normalize();
 
