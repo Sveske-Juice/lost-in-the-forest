4e3c9955c01fa06cb361c3043e385076aee8a9ec
Tagrende <markus37501@gmail.com>
2024-03-06 11:29:11 +0100

    ui

--- Assets/Scripts/Inventory/ToolTip.cs
+++ Assets/Scripts/Inventory/ToolTip.cs
@@ -5,12 +5,14 @@ using TMPro;
 
 public class ToolTip : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI title, description;
+    [SerializeField] TextMeshProUGUI title, description, lvl;
 
     public void set(InventoryItem item)
     {
         title.text = item.data.DisplayName;
-       description.text = item.data.Description;
+        description.text = item.data.Description;
+        lvl.text = "lvl: " + item.data.Level.ToString();
+
     }
 
 
