3458bfa8664daded67ca8865aa70b79f15e90b3a
Mojjedrengen <99266094+Mojjedrengen@users.noreply.github.com>
2024-03-12 10:46:38 +0100

    Lavet items som stod i item cyclen og fikset move speed modifier

--- Assets/Scripts/AbilitySystem/Modifiers/MovementSpeedModifier.cs
+++ Assets/Scripts/AbilitySystem/Modifiers/MovementSpeedModifier.cs
@@ -9,7 +9,7 @@ public interface IMoveSpeedReceiver
 [CreateAssetMenuAttribute(menuName = "Modifier/Move Speed Modifier", fileName = "Move Speed Modifier")]
 public class MovementSpeedModifier : Modifier
 {
-    [Header("MoveSpeedModifier"), Range(-100, 100)]
+    //[Header("MoveSpeedModifier"), Range(-100, 100)]
     [SerializeField] AnimationCurve moveSpeed;
     public override void Apply(UseModifierContext context)
     {
