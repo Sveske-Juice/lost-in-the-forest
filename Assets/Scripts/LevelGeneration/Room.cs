21922b122ff07d6e7852ee645d1bb5449d60d006
SpoodyTheOne <sxsgamer@gmail.com>
2024-03-19 09:26:13 +0100

    Added event to room enter for boss n shit idk

--- Assets/Scripts/LevelGeneration/Room.cs
+++ Assets/Scripts/LevelGeneration/Room.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Room : MonoBehaviour
 {
@@ -9,8 +10,26 @@ public class Room : MonoBehaviour
 
     public uint id = 0;
 
+    public UnityEvent OnRoomEntered;
+
+    public bool isCleared = false;
+
     public void setup() {
         id = (uint)Random.RandomRange(0, 99999999);
         gameObject.name = "Room " + id;
+    }
+
+    public void enter() {
+        if (!isCleared) {
+            OnRoomEntered.Invoke();
+
+            foreach (Door doo in doors) {
+                if (doo.enabled) {
+                    doo.close();
+                }
+            }
+
+            // todo: do logic for enemenemienies
+        }
     }
 }
