b42bbf119d539e09362e16b7c13951cbf78b4eb4
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2024-03-20 21:28:48 +0100

    pause menu buttons & actually stop simulating in pause

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,32 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pausemenu;
-    private bool isPaused = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if(!isPaused)
-            {
-                isPaused = true;
-                pausemenu.SetActive(true);
-            }
-            else if (isPaused)
-            {
-                isPaused = false;
-                pausemenu.SetActive(false);
-            }
+            pausemenu.SetActive(!pausemenu.activeSelf);
         }
+
+        float timeScale = pausemenu.activeSelf ? 0f : 1f;
+        Time.timeScale = timeScale;
     }
+
+    public void Enable() => pausemenu.SetActive(true);
+    public void Disable() => pausemenu.SetActive(false);
 }
