852af55eea0d77ab59be4c6a2dc9ee69089e8eef
Sveske Juice <carl.benjamin.dreyer@gmail.com>
2024-03-20 11:17:38 +0100

    Customizable bullet spawner (bullet sequences, arcs, burst shooting etc.)

---
+++ Assets/Scripts/Combat/BulletSpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    [SerializeField] private float delayBetweenBulletSequences = 0.1f;
+
+    public void RunBulletSequence(AttackContext attackCtx, IEnumerable<BulletPattern> bulletSequence)
+    {
+        StartCoroutine(RunBulletSequenceCourotine(attackCtx, bulletSequence));
+    }
+
+    private IEnumerator RunBulletSequenceCourotine(AttackContext attackCtx, IEnumerable<BulletPattern> bulletSequence)
+    {
+        Vector3 facingDir = attackCtx.attackDir;
+        facingDir.z = 0;
+        facingDir.Normalize();
+
+        foreach (BulletPattern bulletPattern in bulletSequence)
+        {
+            float stepSize = bulletPattern.Arc / ((float)bulletPattern.Amount);
+            
+            for (int i = 0; i < bulletPattern.Amount; i++)
+            {
+                Vector2 bulletDir = rotate(facingDir, stepSize * i + stepSize / 2f - bulletPattern.Arc / 2f);
+
+                GameObject projectile = Instantiate(bulletPattern.bulletPrefab, attackCtx.origin.position, Quaternion.identity);
+                Debug.DrawRay(attackCtx.origin.position, bulletDir, Color.magenta, 5f);
+
+                projectile.transform.up = bulletDir;
+                projectile.GetComponent<Rigidbody2D>().AddForce(bulletDir * bulletPattern.Speed);
+                RangedAttack rangeAttack = projectile.GetComponent<RangedAttack>();
+                rangeAttack.attackCtx = attackCtx;
+
+                if (bulletPattern.Burst)
+                    yield return new WaitForSeconds(bulletPattern.BurstDelay);
+            }
+            yield return new WaitForSeconds(delayBetweenBulletSequences);
+        }
+    }
+
+    public static Vector2 rotate(Vector2 v, float deg)
+    {
+        float delta = deg * (float)Mathf.Deg2Rad;
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
+    }
+}
--- Assets/Scripts/Combat/ProjectileStrategy.cs
+++ Assets/Scripts/Combat/ProjectileStrategy.cs
@@ -1,25 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+
+[System.Serializable]
+public class BulletPattern
+{
+    [SerializeField]
+    public float Arc = 15;
+    [SerializeField]
+    public int Amount = 3;
+    [SerializeField]
+    public float Speed = 1;
+    [SerializeField]
+    public GameObject bulletPrefab;
+
+    [SerializeField]
+    public bool Burst = true;
+    [SerializeField]
+    public float BurstDelay = 0.1f;
+}
+
 
 [CreateAssetMenu(menuName = "Attacks/projectile", fileName = "ProjectileStrategy")]
 public class ProjectileStrategy : AttackStrategy
 {
-    [SerializeField] private GameObject projectilePrefab;
-    [SerializeField] private float projectileSpeed = 1f;
-
-    // TODO: burst functionality (ex. spawn 3 projectiles)
+    [SerializeField] private BulletPattern[] bulletSequence;
 
     public override void Attack(AttackContext _context)
     {
-        Vector3 dir = _context.attackDir;
-        dir.z = 0;
-        dir.Normalize();
 
-        GameObject projectile = Instantiate(projectilePrefab, _context.origin.position, Quaternion.identity);
-        Debug.DrawRay(_context.origin.position, dir, Color.magenta, 5f);
 
-        projectile.transform.up = dir;
-        projectile.GetComponent<Rigidbody2D>().AddForce(dir * projectileSpeed);
-        RangedAttack rangeAttack = projectile.GetComponent<RangedAttack>();
-        rangeAttack.attackCtx = _context;
+        // TODO: move bullet spawner to AttackContext
+        BulletSpawner bulletSpawner = _context.initiator.Transform.gameObject.GetComponent<BulletSpawner>();
+        Assert.IsNotNull(bulletSpawner, $"Bullet spawner needs to be on initiatio {_context.initiator.Transform.name}");
+
+        bulletSpawner.RunBulletSequence(_context, bulletSequence);
+
+        //GameObject projectile = Instantiate(projectilePrefab, _context.origin.position, Quaternion.identity);
+        //Debug.DrawRay(_context.origin.position, dir, Color.magenta, 5f);
+
+        //projectile.transform.up = dir;
+        //projectile.GetComponent<Rigidbody2D>().AddForce(dir * projectileSpeed);
+        //RangedAttack rangeAttack = projectile.GetComponent<RangedAttack>();
+        //rangeAttack.attackCtx = _context;
     }
 }
