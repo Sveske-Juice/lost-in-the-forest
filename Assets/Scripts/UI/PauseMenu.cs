5a1f2856bc85b26abf704784e73d68e0c2b86224
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2024-03-30 12:43:13 +0100

    shop in game & credit display

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pausemenu;
+    public GameObject shopMenu;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             pausemenu.SetActive(!pausemenu.activeSelf);
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            shopMenu.SetActive(!shopMenu.activeSelf);
         }
 
         float timeScale = pausemenu.activeSelf ? 0f : 1f;
