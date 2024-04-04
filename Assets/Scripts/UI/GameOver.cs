c25eaa8f79158384b93cafcaca2ad8cdce196020
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2024-04-04 18:56:44 +0200

    Game over screen

---
+++ Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public float timeToTemple = 5f;
+    public GameObject content;
+
+    public void GameOverStart()
+    {
+        StartCoroutine(GameOverRoutine());
+    }
+
+    private IEnumerator GameOverRoutine()
+    {
+        content.SetActive(true);
+        Time.timeScale = 0f;
+
+        yield return new WaitForSeconds(timeToTemple);
+
+        content.SetActive(false);
+        CombatPlayer.combatPlayer.GetComponent<HealthComponent>().SimpleKill();
+
+        // reset game
+        // WARNING: this mf code is so bad and smelly but i really dont give a FUCk
+        foreach (var go in GameObject.FindObjectsByType<GameObject>(FindObjectsSortMode.None))
+        {
+            if (go != null && go != this.gameObject)
+                Destroy(go);
+        }
+        Destroy(this.gameObject);
+
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0); // main menu
+    }
+}
