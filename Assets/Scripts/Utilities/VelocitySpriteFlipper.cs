cfeeca6ccf35a7555497d6943147ee7c3fbc5392
Sveske Juice <carl.benjamin.dreyer@gmail.com>
2024-04-05 12:36:57 +0200

    Mecha wolf dude

--- Assets/Scripts/Utilities/VelocitySpriteFlipper.cs
+++ Assets/Scripts/Utilities/VelocitySpriteFlipper.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class VelocitySpriteFlipper : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
+    public float moveThreshold = 1.0f;
 
     private Vector3 prevPos = Vector3.zero;
 
     private void Update()
     {
         Vector3 velocity = transform.position - prevPos;
+        if (velocity.magnitude < moveThreshold) return;
         spriteRenderer.flipX = velocity.x >= 0f ? false : true;
 
         prevPos = transform.position;
